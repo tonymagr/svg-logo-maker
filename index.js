f50cef83e5d5e3f0af36d656e6cc8e8f7159a013
Tony Magrady <tonymagrady@gmail.com>
2023-12-01 23:45:23 -0800

    challenge 10- main index and all library code

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const inquirer = require('inquirer');
+const getInput = require('./lib/userInput');
+const writeSvgFile = require('./lib/writeFile');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const logoFile = './examples/logo.svg';
+
+async function inputBuildWrite () {
+    let shape;
+    // Receive inputs
+    let userInputs = await getInput();
+    console.log('userInputs from index script', userInputs);
+
+    // Build shapes
+    if (userInputs.svgShape === 'circle') {
+        shape = new Circle(userInputs.shapeColor, userInputs.svgText, userInputs.textColor);
+    } else if (userInputs.svgShape === 'triangle') {
+        shape = new Triangle(userInputs.shapeColor, userInputs.svgText, userInputs.textColor);
+    } else {
+        shape = new Square(userInputs.shapeColor, userInputs.svgText, userInputs.textColor);
+    }
+
+    // console.log(shape.svgContent.getAttribute('fill'));
+
+    // Create SVG file - write to file
+    writeSvgFile(logoFile, shape.svgContent);
+}
+
+inputBuildWrite();
